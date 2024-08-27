021598953170b269676cda809a6253918fc2c130
Jullianag <juh_g@hotmail.com>
2024-08-27 13:59:30 -0300

    criando mongodb

---
+++ src/main/java/org/meuprojeto/ecommerce_mongodb/EcommerceMongoDbApplication.java
@@ -0,0 +1,13 @@
+package org.meuprojeto.ecommerce_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMongoDbApplication.class, args);
+	}
+
+}
